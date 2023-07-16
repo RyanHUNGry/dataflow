64ec8fba4b5cbe47f62c12e5a152ccd0e680bd3a
RyanHUNGry <ryhung@ucsd.edu>
2023-07-16 12:18:23 -0700

    int test

--- backend/tests/integration/users.test.js
+++ backend/tests/integration/users.test.js
@@ -15,37 +15,53 @@ describe("Users controllers", () => {
     beforeEach(async function () {
         // Connect to test database
         await db.migrate.latest(config)
-        console.log("Starting db")
     });
 
     afterEach(async function () {
         // Rollback test database
         await db.migrate.rollback(config)
-        console.log('closing db')
     });
 
     describe("User signup", () => {
         it('should return status 400 if any fields are missing', async () => {
             const user = {
-              email: "ryan.jake@gmail.net",
-              firstName: "ryan",
-              lastName: "",
-              password: "ryan4ever"
+                email: "ryan.jake@gmail.net",
+                firstName: "ryan",
+                lastName: "",
+                password: "ryan4ever"
             };
-            
-            const response = await (chai.request(app).post('/users/signup').send(user))
-            expect(response).to.have.status(400)
-          })
 
-          it('should return status 201 with newly created user', async () => {
-            const user = {
-              email: "ryan.jake@gmail.net",
-              firstName: "ryan",
-              lastName: "jake",
-              password: "ryan4ever"
-            };
-            
             const response = await (chai.request(app).post('/users/signup').send(user))
+
+            expect(response).to.have.status(400)
+            expect(response.text).to.equal(JSON.stringify({error: "Please specify all fields"}));
+        })
+
+        it('should return status 400 if email already exists', async () => {
+            const user = {
+                email: "ryan.jake@gmail.net",
+                firstName: "ryan",
+                lastName: "jake",
+                password: "ryan4ever"
+            };
+
+            await chai.request(app).post('/users/signup').send(user)
+
+            const response = await chai.request(app).post('/users/signup').send(user)
+            expect(response).to.have.status(400)
+            expect(response.text).to.equal(JSON.stringify({error: "Email already exists"}));
+        })
+
+        it('should return status 201 with newly created user', async () => {
+            const user = {
+                email: "ryan.jake@gmail.net",
+                firstName: "ryan",
+                lastName: "jake",
+                password: "ryan4ever"
+            };
+
+            const response = await (chai.request(app).post('/users/signup').send(user))
+            expect(response).to.have.status(201)
             expect(response._body).to.have.property('uid').to.be.a('number');
             expect(response._body).to.have.property('email').to.equal(user.email);
             expect(response._body).to.have.property('first_name').to.equal(user.firstName);
@@ -53,6 +69,6 @@ describe("Users controllers", () => {
             expect(response._body).to.have.property('created_at').to.be.a('string');
             expect(response._body).to.have.property('updated_at').to.be.a('string');
             expect(response._body).to.have.property('password').to.be.a('string')
-          })
+        })
     })
 })
